8e369b1f21d93feeecdda3f941fa8d1c5aeb5559
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-12-09 14:58:59 +0100

    refactored

--- src/sudokusolver.py
+++ src/sudokusolver.py
@@ -27,6 +27,7 @@ class SudokuSolver:
                                           for j in range(int(self.N ** 2))]
                                          for i in range(int(self.N ** 2))]
 
+    def getConstraint0(self):
         # Setting the known variables of the boolean matrix to true
         # Constraint (C0) : some cells have a known value that can not be changed
         constraint_0 = []
@@ -46,10 +47,11 @@ class SudokuSolver:
                             "Error: not '0' nor belonging to {1-9}U{A-Z}")  # After parsing we should get such a matrix
                         exit()
         print("Constraint 0 : " + str(constraint_0))
-
         print("Known matrix : " + str(self.boolean_variables_matrix))
         print("Known matrix size : " + str(len(self.boolean_variables_matrix)))
+        return constraint_0
 
+    def getConstraint1(self):
         # Constraint (C1) : every cell (i,j) has at least a value Xijk true
         constraint_1 = []
         for i in range(self.N ** 2):
@@ -61,7 +63,9 @@ class SudokuSolver:
 
         #print("len constraint_1 : " + str(len(constraint_1)))
         constraint_1 = And(constraint_1)
+        return constraint_1
 
+    def getConstraint2(self):
         # Constraint (C2) : every cell (i,j) has at most one variable Xijk true
         constraint_2 = []
         for i in range(self.N ** 2):
@@ -86,7 +90,9 @@ class SudokuSolver:
 
         constraint_2 = And(constraint_2)
         # print("Constraint 2 : " + str(constraint_2))
+        return constraint_2
 
+    def getConstraint3(self):
         # Constraint (C3) : every digit from {1-N²} appears in a column
         constraint_3 = []
         for j in range(self.N ** 2):
@@ -101,7 +107,9 @@ class SudokuSolver:
 
         constraint_3 = And(constraint_3)
         # print("Constraint_3 : " + str(constraint_3))
+        return constraint_3
 
+    def getConstraint4(self):
         # Constraint (C4) : every digit from {1-N²} appears in a line
         constraint_4 = []
         for i in range(self.N ** 2):
@@ -116,9 +124,22 @@ class SudokuSolver:
 
         constraint_4 = And(constraint_4)
         # print("Constraint_4 : " + str(constraint_4))
+        return constraint_4
 
+    def getConstraint5(self):
+        # Constraint (C5) : every digit from {1-N²} appears in a square
+        constraint_5 = []
+
+    def solve(self):
         # Solving
         s = Solver()
+
+        constraint_0 = self.getConstraint0()
+        constraint_1 = self.getConstraint1()
+        constraint_2 = self.getConstraint2()
+        constraint_3 = self.getConstraint3()
+        constraint_4 = self.getConstraint4()
+
         s.add(constraint_0)
         s.add(constraint_1)
         s.add(constraint_2)
@@ -153,4 +174,5 @@ class SudokuSolver:
             print(solution_matrix[i])
 
 
-sudokuSolver = SudokuSolver('../test/sudoku_puzzle_2.txt')
+sudokuSolver = SudokuSolver('../test/sudoku_puzzle_4x4_2.txt')
+sudokuSolver.solve()
