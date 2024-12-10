480c1f7d2353f6d686bd96a97edfb7cc3204dd73
Apiram <67603873+apiramaruchunarajah@users.noreply.github.com>
2024-12-10 15:13:02 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import readline, glob
+def complete(text, state):
+    return (glob.glob(text+'*')+[None])[state]
+
+readline.set_completer_delims(' \t\n;')
+readline.parse_and_bind("tab: complete")
+readline.set_completer(complete)
+raw_input('file? ')
