37415dfb389adb8b3aac7287de077d5075a6a1d7
Apiram Aruchunarajah <apiram.aruchunarajah@etu.univ-grenoble-alpes.fr>
2024-12-10 14:27:24 +0100

    added comments

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from src.sudokusolver import SudokuSolver
 
 # Check if an argument is provided
 if len(sys.argv) > 1:
-    pathname = sys.argv[1]  # First argument after the script name
+    pathname = sys.argv[1]
 else:
     print("No arguments were provided.")
     exit()
 
+# Solve the Sudoku
 sudokuSolver = SudokuSolver(pathname)
 print(f"Solving for the sudoku : {pathname} ...")
 sudokuSolver.solve()
